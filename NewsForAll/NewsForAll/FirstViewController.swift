fa77da3117c45929a9e6ff11895b5943456c65ae
Ravindu Senevirathna <ravindu@microimage.com>
2019-12-12 21:43:23 +0530

    init project, add tabs, add pod

---
+++ NewsForAll/NewsForAll/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  newsforall
+//
+//  Created by user on 12/12/19.
+//  Copyright © 2019 MIHCM. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NewsForAll/NewsForAll/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  newsforall
+//
+//  Created by user on 12/12/19.
+//  Copyright © 2019 MIHCM. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
